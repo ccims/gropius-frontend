2ad64f8f296fa7a05ec99a034d3c4ad8bbbcaa15
nk-coding <niklask.coding@gmail.com>
2025-04-20 19:58:30 +0200

    macos fix 2.0?

--- packages/graph-editor/src/views/contextMenuView.ts
+++ packages/graph-editor/src/views/contextMenuView.ts
@@ -2,17 +2,18 @@ import { injectable } from "inversify";
 import { VNode } from "snabbdom";
 import { IView, RenderingContext, html, svg } from "sprotty";
 import { SContextMenu } from "../smodel/sContextMenu.js";
+import { SRoot } from "../smodel/sRoot.js";
 
 @injectable()
 export class ContextMenuView implements IView {
     render(model: Readonly<SContextMenu>, context: RenderingContext, args?: {} | undefined): VNode | undefined {
         const pos = model.pos;
-
+        const zoom = (model.root as SRoot).zoom;
         return svg(
             "g",
             {
-                style: {
-                    transform: `translate(${pos.x}px, ${pos.y}px) scale(calc(1 / var(--diagram-zoom)))`
+                attrs: {
+                    transform: `translate(${pos.x}, ${pos.y}) scale(${1 / zoom})`
                 }
             },
             svg(
