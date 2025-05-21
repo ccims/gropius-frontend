a9ebd7393e8ee10ae6676583608f2d170e214ff7
Flo <tectoast@gmail.com>
2025-05-21 12:21:03 +0200

    feat: base for issue dropdown filters (with labels already implemented)

--- src/views/issue/Issues.vue
+++ src/views/issue/Issues.vue
@@ -5,7 +5,7 @@
         :sort-fields="issueSortFields"
         :to="(issue: Issue) => issueRoute(issue)"
         :sort-ascending-initially="false"
-        :dependencies="[stateFilterInput]"
+        :dependencies="[stateFilterInput, issueLabel]"
         query-param-prefix=""
     >
         <template #item="{ item }">
@@ -13,28 +13,49 @@
         </template>
         <template #search-append>
             <IssueStateSegmentedButton v-model="issueStateIndices" class="ml-2" />
+            <IssueDropdown v-model:selected="issueLabel" :load-options="loadAllLabels" />
         </template>
         <IssueDialogs />
     </PaginatedList>
 </template>
 <script lang="ts" setup>
 import { NodeReturnType, useClient } from "@/graphql/client";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { RouteLocationRaw, useRoute, useRouter } from "vue-router";
 import PaginatedList, { ItemManager } from "@/components/PaginatedList.vue";
-import { IssueListItemInfoFragment, IssueOrder, IssueOrderField } from "@/graphql/generated";
+import {
+    IssueListItemInfoFragment,
+    IssueOrder,
+    IssueOrderField,
+    LabelListFilterInput,
+    LabelOrderField,
+    OrderDirection
+} from "@/graphql/generated";
 import IssueListItem from "@/components/IssueListItem.vue";
 import IssueStateSegmentedButton from "@/components/input/IssueStateSegmentedButton.vue";
 import { IdObject } from "@/util/types";
 import IssueDialogs from "@/components/IssueDialogs.vue";
 import { issueSortFields } from "@/util/issueSortFields";
+import IssueDropdown from "@/components/IssueDropdown.vue";
 
 type Trackable = NodeReturnType<"getIssueList", "Component">;
+type TrackableLabel = NodeReturnType<"getLabelList", "Component">;
 type Issue = IssueListItemInfoFragment;
 
 const client = useClient();
 const router = useRouter();
 const route = useRoute();
+
+const loadAllLabels = async () => {
+    const res = await client.getLabelList({
+        count: 100,
+        orderBy: { field: LabelOrderField.Name, direction: OrderDirection.Asc },
+        skip: 0,
+        trackable: trackableId.value
+        // TODO: whats trackable? and what values should be used here for count and skip?
+    });
+    return (res.node as TrackableLabel).labels.nodes.map((label) => label.name);
+};
 
 const issueStateIndices = computed({
     get: () => {
@@ -52,6 +73,14 @@ const issueStateIndices = computed({
         router.replace({ query: { ...route.query, state } });
     }
 });
+const issueLabel = computed({
+    get: () => {
+        return route.query.labels as string | undefined;
+    },
+    set: (value: string | undefined) => {
+        router.replace({ query: { ...route.query, labels: value } });
+    }
+});
 
 const stateFilterInput = computed(() => {
     if (issueStateIndices.value.length != 1) {
@@ -59,6 +88,12 @@ const stateFilterInput = computed(() => {
     }
     const state = issueStateIndices.value[0] == 0;
     return { isOpen: { eq: state } };
+});
+const labelFilterInput = computed(() => {
+    if (!issueLabel.value) {
+        return undefined;
+    }
+    return { any: { name: { eq: issueLabel.value } } } as LabelListFilterInput;
 });
 
 const trackableId = computed(() => route.params.trackable as string);
@@ -76,7 +111,7 @@ const itemManager: ItemManager<Issue, IssueOrderField> = {
                 count,
                 skip: page * count,
                 trackable: trackableId.value,
-                filter: { state: stateFilterInput.value }
+                filter: { state: stateFilterInput.value, labels: labelFilterInput.value }
             });
             const issues = (res.node as Trackable).issues;
             return [issues.nodes, issues.totalCount];
@@ -84,7 +119,11 @@ const itemManager: ItemManager<Issue, IssueOrderField> = {
             const res = await client.getFilteredIssueList({
                 query: filter,
                 count,
-                filter: { trackables: { any: { id: { eq: trackableId.value } } }, state: stateFilterInput.value }
+                filter: {
+                    trackables: { any: { id: { eq: trackableId.value } } },
+                    state: stateFilterInput.value,
+                    labels: labelFilterInput.value
+                }
             });
             return [res.searchIssues, res.searchIssues.length];
         }
