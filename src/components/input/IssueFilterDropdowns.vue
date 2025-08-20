a9c75bf8d1a88e5fb5f2a4466f1350b7a85f86ef
Flo <tectoast@gmail.com>
2025-08-20 15:31:06 +0200

    refactor(advancedfilters): move all issue filters into one component

---
+++ src/components/input/IssueFilterDropdowns.vue
@@ -0,0 +1,252 @@
+<template>
+    <div class="d-flex ga-4 align-center h-100">
+        <FilterDropdown
+            v-model="templateIds"
+            label="Template"
+            :item-manager="itemManager"
+            :mapper="(item) => item.template"
+            :fetch-on-search="
+                async (search: string) =>
+                    client
+                        .getUsedIssueTemplates({ trackable: trackableId, filter: search })
+                        .then(
+                            (res) =>
+                                (res.node as NodeReturnType<'getUsedIssueTemplates', 'Component'>).usedIssueTemplates
+                                    .nodes
+                        )
+            "
+        />
+        <FilterDropdown
+            v-model="labelIds"
+            label="Label"
+            :item-manager="itemManager"
+            :mapper="(item) => item.labels.nodes"
+            :additional-initial-values="
+                async () =>
+                    client
+                        .firstTrackableLabels({ trackable: trackableId, count: 100 })
+                        .then((res) => (res.node as NodeReturnType<'firstTrackableLabels', 'Component'>).labels.nodes)
+            "
+            :fetch-on-search="
+                async (search: string) =>
+                    client
+                        .getUsedLabels({ trackable: trackableId, filter: search })
+                        .then((res) => (res.node as NodeReturnType<'getUsedLabels', 'Component'>).usedLabels.nodes)
+            "
+        >
+            <template #default="{ item }">
+                <!-- TODO improve typing -->
+                <div class="d-flex align-center">
+                    <v-icon :color="(item as any).color" class="opacity-100 mr-1" icon="mdi-circle" />
+                    <span>{{ item.name }}</span>
+                    <v-chip
+                        color="primary"
+                        size="small"
+                        class="ml-2 flex-shrink-0"
+                        v-if="(item as any).trackables.nodes[0].id !== trackableId"
+                    >
+                        {{ (item as any).trackables.nodes[0].name }}
+                        <v-tooltip
+                            v-if="(item as any).trackables.nodes[0].description"
+                            activator="parent"
+                            location="bottom"
+                        >
+                            {{ (item as any).trackables.nodes[0].description }}
+                        </v-tooltip>
+                    </v-chip>
+                </div>
+            </template>
+        </FilterDropdown>
+        <FilterDropdown
+            v-model="priorityIds"
+            label="Priority"
+            :item-manager="itemManager"
+            :mapper="(item) => item.priority"
+            :sorter="(a, b) => a.value - b.value"
+            :fetch-on-search="
+                async (search: string) =>
+                    client
+                        .getUsedIssuePriorities({ trackable: trackableId, filter: search })
+                        .then(
+                            (res) =>
+                                (res.node as NodeReturnType<'getUsedIssuePriorities', 'Component'>).usedIssuePriorities
+                                    .nodes
+                        )
+            "
+        />
+        <FilterDropdown
+            v-model="typeIds"
+            label="Type"
+            :item-manager="itemManager"
+            :mapper="(item) => item.type"
+            :fetch-on-search="
+                async (search: string) =>
+                    client
+                        .getUsedIssueTypes({ trackable: trackableId, filter: search })
+                        .then(
+                            (res) => (res.node as NodeReturnType<'getUsedIssueTypes', 'Component'>).usedIssueTypes.nodes
+                        )
+            "
+        >
+            <template #default="{ item }">
+                <div class="d-flex align-center">
+                    <IssueTypeIcon :path="item.iconPath" fill="rgb(var(--v-theme-primary))" class="type-icon mr-1" />
+                    {{ item.name }}
+                </div>
+            </template>
+        </FilterDropdown>
+        <FilterDropdown
+            v-model="assignedToIds"
+            label="Assigned to"
+            :item-manager="itemManager"
+            :mapper="
+                (item) =>
+                    item.assignments.nodes.map((node) => {
+                        const name = node.user.id == store.user?.id ? 'Me' : node.user.displayName;
+                        return {
+                            ...node.user,
+                            displayName: name,
+                            name
+                        };
+                    })
+            "
+            :sorter="assignedToSorter"
+            :fetch-on-search="
+                async (search: string) =>
+                    client.getAssignedUsers({ trackable: trackableId, filter: search }).then((res) =>
+                        (res.node as NodeReturnType<'getAssignedUsers', 'Component'>).assignedUsers.nodes.map(
+                            (node) => {
+                                const name = node.id == store.user?.id ? 'Me' : node.displayName;
+                                return {
+                                    ...node,
+                                    displayName: name,
+                                    name
+                                };
+                            }
+                        )
+                    )
+            "
+        >
+            <template #default="{ item }">
+                <User :user="item" />
+            </template>
+        </FilterDropdown>
+        <FilterDropdown
+            v-model="stateIds"
+            label="State"
+            :mapper="(item) => item.state"
+            :item-manager="itemManager"
+            :fetch-on-search="
+                async (search: string) =>
+                    client
+                        .getUsedIssueStates({ trackable: trackableId, filter: search })
+                        .then(
+                            (res) =>
+                                (res.node as NodeReturnType<'getUsedIssueStates', 'Component'>).usedIssueStates.nodes
+                        )
+            "
+        >
+            <template #default="{ item }">
+                <span class="d-flex align-center">
+                    <v-icon
+                        :color="item.isOpen ? 'issue-open' : 'issue-closed'"
+                        class="opacity-100 mr-1"
+                        icon="mdi-circle"
+                    />
+                    {{ item.name }}
+                </span>
+            </template>
+        </FilterDropdown>
+    </div>
+</template>
+
+<script setup lang="ts" generic="T extends IssueListItemInfoFragment, S extends IssueOrderField">
+import { NodeReturnType, useClient } from "@/graphql/client";
+import FilterDropdown from "@/components/input/FilterDropdown.vue";
+import IssueTypeIcon from "@/components/IssueTypeIcon.vue";
+import User from "@/components/info/User.vue";
+import { computed, PropType } from "vue";
+import { ItemManager } from "@/util/itemManager";
+import { IssueListItemInfoFragment, IssueOrderField } from "@/graphql/generated";
+import { useFilterOption } from "@/util/useFilterOption";
+import { IdObject } from "@/util/types";
+import { useAppStore } from "@/store/app";
+
+defineProps({
+    itemManager: {
+        type: Object as PropType<ItemManager<T, S>>,
+        required: true
+    },
+    trackableId: {
+        type: String,
+        required: true
+    }
+});
+
+const client = useClient();
+const store = useAppStore();
+
+const templateIds = useFilterOption("template");
+const templateInput = computed(() => {
+    return templateIds.value.length > 0 ? { id: { in: templateIds.value } } : undefined;
+});
+const labelIds = useFilterOption("label");
+const labelInput = computed(() => {
+    return labelIds.value.length > 0 ? { any: { id: { in: labelIds.value } } } : undefined;
+});
+const priorityIds = useFilterOption("priority");
+const priorityInput = computed(() => {
+    return priorityIds.value.length > 0 ? { id: { in: priorityIds.value } } : undefined;
+});
+const typeIds = useFilterOption("type");
+const typeInput = computed(() => {
+    return typeIds.value.length > 0 ? { id: { in: typeIds.value } } : undefined;
+});
+const assignedToIds = useFilterOption("assignedTo");
+const assignedToInput = computed(() => {
+    return assignedToIds.value.length > 0
+        ? {
+              any: {
+                  user: {
+                      id: {
+                          in: assignedToIds.value
+                      }
+                  }
+              }
+          }
+        : undefined;
+});
+const assignedToSorter = (a: IdObject & { name: string }, b: IdObject & { name: string }) => {
+    const isAssignedToMeA = store.user?.id == a.id;
+    const isAssignedToMeB = store.user?.id == b.id;
+    if (isAssignedToMeA && !isAssignedToMeB) return -1;
+    if (!isAssignedToMeA && isAssignedToMeB) return 1;
+    return a.name.localeCompare(b.name);
+};
+
+const stateIds = useFilterOption("concretestate");
+
+const dependencyArray = computed(() => {
+    return [templateIds, labelIds, priorityIds, typeIds, assignedToIds, stateIds];
+});
+
+defineExpose({
+    templateInput,
+    labelInput,
+    priorityInput,
+    typeInput,
+    assignedToInput,
+    stateIds,
+    dependencyArray
+});
+</script>
+
+<style scoped lang="scss">
+@use "@/styles/settings.scss";
+@use "sass:map";
+
+.type-icon {
+    width: map.get(settings.$avatar-sizes, "large");
+    height: map.get(settings.$avatar-sizes, "large");
+}
+</style>
--- src/views/issue/Issues.vue
+++ src/views/issue/Issues.vue
@@ -5,7 +5,7 @@
         :sort-fields="issueSortFields"
         :to="(issue: Issue) => issueRoute(issue)"
         :sort-ascending-initially="false"
-        :dependencies="[stateFilterInput, templateInput, labelInput, assignedToInput, priorityInput, typeInput]"
+        :dependencies="[stateFilterInput, ...(filterFromDropdown?.dependencyArray ?? [])]"
         query-param-prefix=""
     >
         <template #item="{ item }">
@@ -15,178 +15,14 @@
             <IssueStateSegmentedButton v-model="issueStateIndices" class="ml-2" />
         </template>
         <template #additional-filter>
-            <div class="d-flex ga-4 align-center h-100">
-                <FilterDropdown
-                    v-model="templateIds"
-                    label="Template"
-                    :item-manager="itemManager"
-                    :mapper="(item) => item.template"
-                    :fetch-on-search="
-                        async (search: string) =>
-                            client
-                                .getUsedIssueTemplates({ trackable: trackableId, filter: search })
-                                .then(
-                                    (res) =>
-                                        (res.node as NodeReturnType<'getUsedIssueTemplates', 'Component'>)
-                                            .usedIssueTemplates.nodes
-                                )
-                    "
-                />
-                <FilterDropdown
-                    v-model="labelIds"
-                    label="Label"
-                    :item-manager="itemManager"
-                    :mapper="(item) => item.labels.nodes"
-                    :additional-initial-values="
-                        async () =>
-                            client
-                                .firstTrackableLabels({ trackable: trackableId, count: 100 })
-                                .then(
-                                    (res) =>
-                                        (res.node as NodeReturnType<'firstTrackableLabels', 'Component'>).labels.nodes
-                                )
-                    "
-                    :fetch-on-search="
-                        async (search: string) =>
-                            client
-                                .getUsedLabels({ trackable: trackableId, filter: search })
-                                .then(
-                                    (res) => (res.node as NodeReturnType<'getUsedLabels', 'Component'>).usedLabels.nodes
-                                )
-                    "
-                >
-                    <template #default="{ item }">
-                        <!-- TODO improve typing -->
-                        <div class="d-flex align-center">
-                            <v-icon :color="(item as any).color" class="opacity-100 mr-1" icon="mdi-circle" />
-                            <span>{{ item.name }}</span>
-                            <v-chip color="primary" size="small" class="ml-2 flex-shrink-0" v-if="(item as any).trackables.nodes[0].id !== trackableId">
-                                {{ (item as any).trackables.nodes[0].name }}
-                                <v-tooltip
-                                    v-if="(item as any).trackables.nodes[0].description"
-                                    activator="parent"
-                                    location="bottom"
-                                >
-                                    {{ (item as any).trackables.nodes[0].description }}
-                                </v-tooltip>
-                            </v-chip>
-                        </div>
-                    </template>
-                </FilterDropdown>
-                <FilterDropdown
-                    v-model="priorityIds"
-                    label="Priority"
-                    :item-manager="itemManager"
-                    :mapper="(item) => item.priority"
-                    :sorter="(a, b) => a.value - b.value"
-                    :fetch-on-search="
-                        async (search: string) =>
-                            client
-                                .getUsedIssuePriorities({ trackable: trackableId, filter: search })
-                                .then(
-                                    (res) =>
-                                        (res.node as NodeReturnType<'getUsedIssuePriorities', 'Component'>)
-                                            .usedIssuePriorities.nodes
-                                )
-                    "
-                />
-                <FilterDropdown
-                    v-model="typeIds"
-                    label="Type"
-                    :item-manager="itemManager"
-                    :mapper="(item) => item.type"
-                    :fetch-on-search="
-                        async (search: string) =>
-                            client
-                                .getUsedIssueTypes({ trackable: trackableId, filter: search })
-                                .then(
-                                    (res) =>
-                                        (res.node as NodeReturnType<'getUsedIssueTypes', 'Component'>).usedIssueTypes
-                                            .nodes
-                                )
-                    "
-                >
-                    <template #default="{ item }">
-                        <div class="d-flex align-center">
-                            <IssueTypeIcon
-                                :path="item.iconPath"
-                                fill="rgb(var(--v-theme-primary))"
-                                class="type-icon mr-1"
-                            />
-                            {{ item.name }}
-                        </div>
-                    </template>
-                </FilterDropdown>
-                <FilterDropdown
-                    v-model="assignedToIds"
-                    label="Assigned to"
-                    :item-manager="itemManager"
-                    :mapper="
-                        (item) =>
-                            item.assignments.nodes.map((node) => {
-                                const name = node.user.id == store.user?.id ? 'Me' : node.user.displayName;
-                                return {
-                                    ...node.user,
-                                    displayName: name,
-                                    name
-                                };
-                            })
-                    "
-                    :sorter="assignedToSorter"
-                    :fetch-on-search="
-                        async (search: string) =>
-                            client.getAssignedUsers({ trackable: trackableId, filter: search }).then((res) =>
-                                (res.node as NodeReturnType<'getAssignedUsers', 'Component'>).assignedUsers.nodes.map(
-                                    (node) => {
-                                        const name = node.id == store.user?.id ? 'Me' : node.displayName;
-                                        return {
-                                            ...node,
-                                            displayName: name,
-                                            name
-                                        };
-                                    }
-                                )
-                            )
-                    "
-                >
-                    <template #default="{ item }">
-                        <User :user="item" />
-                    </template>
-                </FilterDropdown>
-                <FilterDropdown
-                    label="State"
-                    :mapper="(item) => item.state"
-                    :item-manager="itemManager"
-                    :fetch-on-search="
-                        async (search: string) =>
-                            client
-                                .getUsedIssueStates({ trackable: trackableId, filter: search })
-                                .then(
-                                    (res) =>
-                                        (res.node as NodeReturnType<'getUsedIssueStates', 'Component'>).usedIssueStates
-                                            .nodes
-                                )
-                    "
-                >
-                    <template #default="{ item }">
-                        <span class="d-flex align-center">
-                            <v-icon
-                                :color="item.isOpen ? 'issue-open' : 'issue-closed'"
-                                class="opacity-100 mr-1"
-                                icon="mdi-circle"
-                            />
-                            {{ item.name }}
-                        </span>
-                    </template>
-                </FilterDropdown>
-            </div>
+            <IssueFilterDropdowns :trackable-id="trackableId" :item-manager="itemManager" ref="filterDropdowns" />
         </template>
         <IssueDialogs />
     </PaginatedList>
 </template>
 <script lang="ts" setup>
 import { NodeReturnType, useClient } from "@/graphql/client";
-import { computed, ref } from "vue";
+import { computed, useTemplateRef } from "vue";
 import { RouteLocationRaw, useRoute, useRouter } from "vue-router";
 import PaginatedList from "@/components/PaginatedList.vue";
 import { IssueFilterInput, IssueListItemInfoFragment, IssueOrder, IssueOrderField } from "@/graphql/generated";
@@ -196,60 +32,16 @@ import { IdObject } from "@/util/types";
 import IssueDialogs from "@/components/IssueDialogs.vue";
 import { issueSortFields } from "@/util/issueSortFields";
 import { ItemManager } from "@/util/itemManager";
-import FilterDropdown from "@/components/input/FilterDropdown.vue";
-import { useFilterOption } from "@/util/useFilterOption";
-import { useAppStore } from "@/store/app";
-import User from "@/components/info/User.vue";
-import IssueTypeIcon from "@/components/IssueTypeIcon.vue";
+import IssueFilterDropdowns from "@/components/input/IssueFilterDropdowns.vue";
 
 type Trackable = NodeReturnType<"getIssueList", "Component">;
-type TrackableLabel = NodeReturnType<"getLabelList", "Component">;
 type Issue = IssueListItemInfoFragment;
 
 const client = useClient();
 const router = useRouter();
 const route = useRoute();
-const store = useAppStore();
 
-const templateIds = useFilterOption("template");
-const templateInput = computed(() => {
-    return templateIds.value.length > 0 ? { id: { in: templateIds.value } } : undefined;
-});
-const labelIds = useFilterOption("label");
-const labelInput = computed(() => {
-    return labelIds.value.length > 0 ? { any: { id: { in: labelIds.value } } } : undefined;
-});
-const priorityIds = useFilterOption("priority");
-const priorityInput = computed(() => {
-    return priorityIds.value.length > 0 ? { id: { in: priorityIds.value } } : undefined;
-});
-const typeIds = useFilterOption("type");
-const typeInput = computed(() => {
-    return typeIds.value.length > 0 ? { id: { in: typeIds.value } } : undefined;
-});
-const assignedToIds = useFilterOption("assignedTo");
-const assignedToInput = computed(() => {
-    return assignedToIds.value.length > 0
-        ? {
-              any: {
-                  user: {
-                      id: {
-                          in: assignedToIds.value
-                      }
-                  }
-              }
-          }
-        : undefined;
-});
-const assignedToSorter = (a: IdObject & { name: string }, b: IdObject & { name: string }) => {
-    const isAssignedToMeA = store.user?.id == a.id;
-    const isAssignedToMeB = store.user?.id == b.id;
-    if (isAssignedToMeA && !isAssignedToMeB) return -1;
-    if (!isAssignedToMeA && isAssignedToMeB) return 1;
-    return a.name.localeCompare(b.name);
-};
-
-const stateIds = useFilterOption("concretestate");
+const filterFromDropdown = useTemplateRef<InstanceType<typeof IssueFilterDropdowns>>("filterDropdowns");
 
 const issueStateIndices = computed({
     get: () => {
@@ -270,14 +62,14 @@ const issueStateIndices = computed({
 
 const stateFilterInput = computed(() => {
     const isSingleIssueState = issueStateIndices.value.length == 1;
-    const customStateSelected = !!stateIds.value.length;
+    const customStateSelected = !!filterFromDropdown.value?.stateIds?.length;
     if (!isSingleIssueState && !customStateSelected) {
         return undefined;
     }
     const state = issueStateIndices.value[0] == 0;
     return {
         isOpen: isSingleIssueState ? { eq: state } : undefined,
-        id: customStateSelected ? { in: stateIds.value } : undefined
+        id: customStateSelected ? { in: filterFromDropdown.value?.stateIds } : undefined
     };
 });
 
@@ -290,14 +82,17 @@ class IssueItemManager extends ItemManager<Issue, IssueOrderField> {
         count: number,
         page: number
     ): Promise<[Issue[], number]> {
-        const generalFilters: Partial<IssueFilterInput> = {
-            labels: labelInput.value,
-            template: templateInput.value,
-            state: stateFilterInput.value,
-            assignments: assignedToInput.value,
-            priority: priorityInput.value,
-            type: typeInput.value
-        };
+        const currentFilter = filterFromDropdown.value;
+        const generalFilters: Partial<IssueFilterInput> = currentFilter
+            ? {
+                  labels: currentFilter.labelInput,
+                  template: currentFilter.templateInput,
+                  assignments: currentFilter.assignedToInput,
+                  priority: currentFilter.priorityInput,
+                  type: currentFilter.typeInput,
+                  state: stateFilterInput.value
+              }
+            : {};
         if (filter == undefined) {
             const res = await client.getIssueList({
                 orderBy,
@@ -331,13 +126,3 @@ function issueRoute(issue: IdObject): RouteLocationRaw {
     };
 }
 </script>
-
-<style scoped lang="scss">
-@use "@/styles/settings.scss";
-@use "sass:map";
-
-.type-icon {
-    width: map.get(settings.$avatar-sizes, "large");
-    height: map.get(settings.$avatar-sizes, "large");
-}
-</style>
